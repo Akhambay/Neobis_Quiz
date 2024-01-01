288e2704f9bbe54184a2faec9dcfe9ec20a984a8
Akhambay <assyl.akhambay@gmail.com>
2024-01-01 17:10:05 +0600

    Add and Update models

---
+++ api/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Quiz(models.Model):
+
+    class Meta:
+        verbose_name = _("Quiz")
+        verbose_name_plural = _("Quizzes")
+        ordering = ['id']
+
+    title = models.ForeignKey(max_length=255, default=_(
+        "New Quiz"), verbose_name="Quiz Title")
+    category = models.ForeignKey(
+        Category, default=1, on_delete=models.DO_NOTHING)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Updated(models.Model):
+    date_updated = models.DateTimeField(
+        verbose_name=_("Last updated"), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Question(Updated):
+
+    class Meta:
+        verbose_name = _("Question")
+        verbose_name_plural = _("Questions")
+        ordering = ['id']
+
+    quiz = models.ForeignKey(
+        Quiz, related_name='question', on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=255, verbose_name="Title")
+    date_created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_("Date created"))
+    is_active = models.BooleanField(
+        default=False, verbose_name=_("Active status"))
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(Updated):
+
+    class Meta:
+        verbose_name = _("Answer")
+        verbose_name_plural = _("Answers")
+        ordering = ['id']
+
+    question = models.ForeignKey(
+        Question, related_name='answer', on_delete=models.DO_NOTHING)
+    answer_text = models.CharField(
+        max_length=255, verbose_name=_("Answer Text"))
+    is_right = models.BooleanField(default="False")
+
+
+class Article(Updated):
+
+    class Meta:
+        verbose_name = _("Article")
+        verbose_name_plural = _("Articles")
+        ordering = ['id']
+
+    title = models.ForeignKey(max_length=255, default=_(
+        "New article"), verbose_name="Article Title")
+    content = models.TextField(
+        max_length=500, verbose_name=_("Article Text"))
+    category = models.ForeignKey(
+        Category, default=1, on_delete=models.DO_NOTHING)
+    date_created = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(
+        default=False, verbose_name=_("Active status"))
